9ee430856c2b606a0563f3c47190d47aab12f760
Ivan Ferro <iaferro@gmail.com>
2019-04-13 11:58:32 -0300

    Aula Enterprise PHP - 13/04/2019 - 11:58

---
+++ HandsOn/Aula03/Aula03_01_funcao_fopen_fread.php
@@ -0,0 +1,36 @@
+<?php
+
+//Cria uma conexao com o arquivo
+$stream = fopen('emails.txt', 'r'); 
+
+// Faz a leitura do arquivo na quantidade de bytes especificada
+echo nl2br(fread($stream, 45));
+
+// Fecha o stream liberando o arquivo
+fclose($stream);
+
+echo '<hr>';
+
+$stream2 = fopen('emails.txt', 'r');
+// 'r'  abre somente para leitura
+// 'r+' abre para leitura e escrita
+// 'w'  abre somente para escrita
+// 'w+' abre para leitura e escrita 
+// 'a'  abre somente para escrita, coloca o ponteiro no final do arquivo
+//      Se o arquivo não existir, tenta cria-lo
+// 'a+' abre para leitrua e escrita; coloca o ponteiro do arquivo
+//      no final do arquivo. Se o arquivo na existir, tenta cria-lo
+
+while (($pedaco = fread($stream2, 45)) != false) {
+    echo nl2br($pedaco);
+}
+
+fclose($stream2);
+
+echo '<br>';
+
+$stream3 = fopen('emails.txt', 'r');
+
+// A funcao filesize retorna o tamanho do arquivo em bytes
+echo nl2br(fread($stream3, filesize('emails.txt')));
+fclose($stream3);
