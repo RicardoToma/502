ded1015480b25f77525ec6788585d0c1dc4fe48a
Ivan Ferro <iaferro@gmail.com>
2019-05-11 14:11:40 -0300

    Aula Enterprise PHP - 11/05/2019 - 14:10

--- HandsOn/Aula08/REST_MVC/App/Lib/FrontController/FrontController.php
+++ HandsOn/Aula08/REST_MVC/App/Lib/FrontController/FrontController.php
@@ -8,23 +8,24 @@ class FrontController
     {
         // teste: index.php?controller=index&action=sair
         $rota = $_SERVER['REQUEST_URI'];
-        
+             
+
         if ($rota != '/') {
             $arrayRota = explode('/', $rota);
         }
-        
+   
         $controller = isset($arrayRota[1]) ? $arrayRota[1] : 'index';
-        
+ 
         $action = isset($arrayRota[2]) ? $arrayRota[2] : 'index';
         
         $_GET['id'] = isset($arrayRota[3]) ? $arrayRota[3] : '';
         
-        // $controller = isset($_GET['controller']) ? $_GET['controller'] : 'index';
+        //  $controller = isset($_GET['controller']) ? $_GET['controller'] : 'index';
         
-        // $action = isset($_GET['action']) ? $_GET['action'] : 'index';
+        //  $action = isset($_GET['action']) ? $_GET['action'] : 'index';
         
         $controller = 'Controller\\' . ucFirst($controller);
-        
+
         $objController = new $controller();
         
         $objController->$action();
